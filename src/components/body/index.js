f8f7320c091d5e4e95f1893e615acffa79d3146c
Potwora <matthewpotwora@yahoo.com>
2022-11-05 12:18:04 -0700

    working on buttons and set up

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 function Body() {
     return(
-        <section> 
-           <div>
-            work on making a search bar and button here matt!!!test 
-           </div>
+        <section class="center"> 
+           <div class="wrapper">
+           <input id="searchbar" onkeyup="search_player()" type="text"
+        class="search" placeholder="Search players.."></input>
+        <button class="button button1">Submit</button>
+        <label for="categories">Choose a stat category:</label>
+        <select name="categories" id="categories">
+<option value="Points">Points</option>
+<option value="Assists">Assists</option>
+<option value="Rebounds">Rebounds</option>
+<option value="Steals">Steals</option>
+<option value="Blocks">Blocks</option>
+        </select>
+        <button class="button button1">Add players info</button>
+        </div>
     </section>
  //uhhhhhh
     );
