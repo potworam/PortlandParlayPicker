84c5145655cdb33af65c7bcb5364290a129896b1
Potwora <matthewpotwora@yahoo.com>
2022-12-10 08:45:45 -0800

    got a button consolelogging

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -9,23 +9,22 @@ function Body() {
         <section class="center"> 
         <div>Type a players name</div>
            <div class="wrapper">
-           <input id="searchbar" onkeyup="search_player()" type="text"
+           <input id="playerName" onkeyup="search_player()" type="text"
         class="search" placeholder="Search for a player"></input>
         {/* above box needs to be a dropdown with players from api, or does it?  */}
-        {/* <button class="button button1">Submit</button> */}
         <label for="categories">Choose a stat category:</label>
-        <select name="categories" id="categories">
+        <select name="categories" id="statCategories">
 <option value="Points">Points</option>
 <option value="Assists">Assists</option>
 <option value="Rebounds">Rebounds</option>
 <option value="Steals">Steals</option>
 <option value="Blocks">Blocks</option>
         </select>
-        <input id="searchbar" onkeyup="search_player()" type="number"
+        <input id="statNumber" onkeyup="search_player()" type="number"
         class="search" placeholder="input expected stat"></input>
-        <button onclick= {createCard}  class="button button1" >Create Stat Card</button>
-        {/* maybe add event listener? */}
-        {/* console.log not working... */}
+        <button onClick= {createCard}  class="button button1" >Create Stat Card</button>
+{/* BUTTON CONSOLE LOG WORKS THIS IS GOOD */}
+{/* go back and look at forms online */}
         </div>
     </section>
     );
